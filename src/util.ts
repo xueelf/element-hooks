2f9b46c0adfa916f3442fd699cf6cd53c1c1bf64
Yuki <admin@yuki.sh>
2026-01-18 10:20:49 +0800

    feat: add useTable composable and utility types

---
+++ src/util.ts
@@ -0,0 +1,28 @@
+import { type Ref } from 'vue';
+
+export type InstanceActions<T> = {
+  [K in keyof T as T[K] extends Function ? K : never]: T[K];
+} & {
+  getInstance(): T;
+};
+
+export function createInstanceActions<T>(ref: Ref): InstanceActions<T> {
+  return new Proxy(<InstanceActions<T>>{}, {
+    get(_, prop) {
+      return (...args: unknown[]) => {
+        if (ref.value === null) {
+          throw new Error(`Component has not been mounted yet`);
+        }
+        if (prop === 'getInstance') {
+          return ref.value;
+        }
+        const property = Reflect.get(ref.value, prop);
+
+        if (typeof property !== 'function') {
+          throw new Error(`${prop.toString()} is not a function`);
+        }
+        return property(...args);
+      };
+    },
+  });
+}
