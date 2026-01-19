a876624d419e58417e4c940bf23d60038746f88f
Yuki <admin@yuki.sh>
2026-01-19 17:08:12 +0800

    feat: add VitePress config and custom theme setup

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,9 @@
+import 'virtual:uno.css';
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+
+export default {
+  extends: DefaultTheme,
+} satisfies Theme;
