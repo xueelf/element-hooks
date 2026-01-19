e49885b7f268176d35efd292dde9000547d88c8d
Yuki <admin@yuki.sh>
2026-01-19 17:08:55 +0800

    style(docs): style Element Plus tables

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import 'virtual:uno.css';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
+import './custom.scss';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 
