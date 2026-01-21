de13deef7d3bfc58483b406332cd92b71f0f3252
Yuki <admin@yuki.sh>
2026-01-21 17:32:14 +0800

    docs(table): add basic example

---
+++ docs/examples/table/BasicTable.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import { useTable } from 'element-hooks';
+
+  const [Table] = useTable({
+    columns: [
+      { prop: 'date', label: 'Date', width: 180 },
+      { prop: 'name', label: 'Name', width: 180 },
+      { prop: 'address', label: 'Address' },
+    ],
+    data: [
+      {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+      {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+      },
+    ],
+  });
+</script>
+
+<template>
+  <Table style="width: 100%" />
+</template>
