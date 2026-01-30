536c5da3bb2bb3190a15cdc608f9e6fb1bfa58c2
Yuki <admin@yuki.sh>
2026-01-30 17:57:11 +0800

    chore: enhance ExampleCard with copy and expand features

--- docs/components/ExampleCard.vue
+++ docs/components/ExampleCard.vue
@@ -1,12 +1,52 @@
 <script setup lang="ts">
-  import { reactive } from 'vue';
+  import { ElMessage } from 'element-plus';
+  import { Check, CopyDocument, Hide, View } from '@element-plus/icons-vue';
+  import { computed, reactive, useTemplateRef } from 'vue';
 
   const state = reactive({
-    isExpanded: false,
+    copied: false,
+    expanded: false,
+    sourceCodeRef: useTemplateRef<HTMLDivElement>('source-code'),
+  });
+  const icon = computed(() => {
+    return {
+      copy: {
+        component: state.copied ? Check : CopyDocument,
+        title: state.copied ? '已复制' : '复制代码',
+      },
+      expand: {
+        component: state.expanded ? Hide : View,
+        title: state.expanded ? '隐藏代码' : '查看代码',
+      },
+    };
   });
 
+  const handleCopy = async () => {
+    if (!state.sourceCodeRef) {
+      return;
+    }
+    const code = state.sourceCodeRef.querySelector('pre code')?.textContent;
+
+    if (!code) {
+      ElMessage.warning('未找到可复制的代码');
+      return;
+    }
+
+    try {
+      await navigator.clipboard.writeText(code);
+
+      ElMessage.success('代码已复制');
+      Reflect.set(state, 'copied', true);
+      setTimeout(() => {
+        Reflect.set(state, 'copied', false);
+      }, 2000);
+    } catch (err) {
+      ElMessage.error('代码复制失败');
+    }
+  };
+
   const handleExpand = () => {
-    state.isExpanded = !state.isExpanded;
+    state.expanded = !state.expanded;
   };
 </script>
 
@@ -16,38 +56,41 @@
       <slot name="example" />
     </template>
     <template #footer>
-      <div
-        class="bg-[--el-card-bg-color] px-[--el-card-padding] flex justify-end items-center gap-4 h-10"
-      >
+      <div class="action-wrapper">
         <el-icon class="cursor-pointer" title="Open in Playground">
           <icon-ep-link />
         </el-icon>
-        <el-icon class="cursor-pointer" title="Copy Code">
-          <icon-ep-copy-document />
+        <el-icon
+          class="cursor-pointer"
+          :title="icon.copy.title"
+          @click="handleCopy"
+        >
+          <component :is="icon.copy.component" />
         </el-icon>
         <el-icon
           class="cursor-pointer"
-          :title="state.isExpanded ? 'Hide Code' : 'Show Code'"
+          :title="icon.expand.title"
           @click="handleExpand"
         >
-          <icon-ep-hide v-if="state.isExpanded" />
-          <icon-ep-view v-else />
+          <component :is="icon.expand.component" />
         </el-icon>
       </div>
-      <div :class="state.isExpanded ? 'block' : 'hidden'">
+      <div ref="source-code" :class="state.expanded ? 'block' : 'hidden'">
         <slot />
-        <el-button
-          link
-          style="width: 100%; height: 2.5rem"
-          @click="handleExpand"
-        >
-          <template #icon>
-            <el-icon class="cursor-pointer">
-              <icon-ep-caret-top />
-            </el-icon>
-          </template>
-          <span>隐藏源代码</span>
-        </el-button>
+        <div class="expand-wrapper">
+          <el-button
+            link
+            style="width: 100%; height: 100%"
+            @click="handleExpand"
+          >
+            <template #icon>
+              <el-icon class="cursor-pointer">
+                <icon-ep-caret-top />
+              </el-icon>
+            </template>
+            <span>{{ icon.expand.title }}</span>
+          </el-button>
+        </div>
       </div>
     </template>
   </el-card>
@@ -55,6 +98,27 @@
 
 <style scoped lang="scss">
   .example-card {
+    overflow: visible;
+
+    .action-wrapper {
+      height: 2.5rem;
+      padding: 0 var(--el-card-padding);
+      background-color: var(--el-card-bg-color);
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      justify-content: flex-end;
+    }
+
+    .expand-wrapper {
+      height: 2.5rem;
+      position: sticky;
+      bottom: 0;
+      background-color: var(--el-card-bg-color);
+      z-index: 1;
+      border-top: 1px solid var(--el-card-border-color);
+    }
+
     :deep(.el-card__footer) {
       padding: 0;
 
