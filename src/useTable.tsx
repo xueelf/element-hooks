edf819c515821cdaef50fc90507b5c1faae3f38b
Yuki <admin@yuki.sh>
2026-01-31 17:41:11 +0800

    refactor: table utility and enhance column slot support

--- src/useTable.tsx
+++ src/useTable.tsx
@@ -4,43 +4,77 @@ import {
   type TableColumnCtx,
   type TableInstance,
 } from 'element-plus';
-import { defineComponent, computed, ref } from 'vue';
-import { createInstanceActions } from './util';
+import { defineComponent, computed, ref, reactive } from 'vue';
+import { createController } from './util';
+
+export type TableColumnSlotName =
+  | 'default'
+  | 'header'
+  | 'filterIcon'
+  | 'expand';
+
+export type ColumnDefaultScope<T extends Recordable> = {
+  column: TableColumnCtx<T>;
+  row: T;
+  $index: number;
+};
+export type ColumnHeaderScope<T extends Recordable> = {
+  column: TableColumnCtx<T>;
+  $index: number;
+};
+export type ColumnFilterIconScope = {
+  filterOpened: boolean;
+};
+export type ColumnExpandScope = {
+  expanded: boolean;
+};
+export type ColumnScope<T extends Recordable> =
+  | ColumnDefaultScope<T>
+  | ColumnHeaderScope<T>
+  | ColumnFilterIconScope
+  | ColumnExpandScope;
 
 export type TableColumn<T extends Recordable> = Partial<
   Omit<TableColumnCtx<T>, 'children'>
 > & {
-  slot?: string;
   children?: TableColumn<T>[];
+  slot?: string;
+  slots?: Partial<Record<TableColumnSlotName, string>>;
 };
-// store, _self, column, row, $index, cellIndex, expanded
-export type ColumnScope<T extends Recordable> = {
-  store: Recordable;
-  _self: Recordable;
-  column: TableColumnCtx<T>;
-  row: T;
-  $index: number;
-  cellIndex: number;
-  expanded: Recordable;
-};
+
 export type TableOptions<T extends Recordable> = Camelized<
-  Omit<TableInstance['$props'], 'ref' | 'data' | 'columns'>
+  Omit<TableInstance['$props'], 'ref'>
 > & {
   data?: T[];
   columns?: TableColumn<T>[];
 };
 
 export function useTable<T extends Recordable = Recordable>(
-  options: TableOptions<T>,
+  options: TableOptions<T> = {},
 ) {
-  const instanceRef = ref<TableInstance | null>(null);
-  const instanceActions = createInstanceActions<TableInstance>(instanceRef);
+  const tableInstance = ref<TableInstance | null>(null);
+  const tableProps = reactive<TableOptions<T>>(options);
 
-  const Table = defineComponent<typeof options>({
+  const setProps = (props: Partial<TableOptions<T>>) => {
+    Object.assign(tableProps, props);
+  };
+  const setColumns = (columns: TableColumn<T>[]) => {
+    setProps({ columns });
+  };
+  const setData = (data: T[]) => {
+    setProps({ data });
+  };
+
+  const tableController = createController(tableInstance, {
+    setProps,
+    setData,
+    setColumns,
+  });
+  const Table = defineComponent<TableOptions<T>>({
     name: 'Table',
     setup(props, { attrs, slots }) {
-      const optionsState = computed(() => {
-        const { columns, ...rest } = options;
+      const tableState = computed(() => {
+        const { columns, ...rest } = tableProps;
 
         return {
           columns,
@@ -48,39 +82,45 @@ export function useTable<T extends Recordable = Recordable>(
         };
       });
       const Column = (columnProps: TableColumn<T>) => {
-        const { slot, children, ...rest } = columnProps;
+        const {
+          slot,
+          slots: columnsSlots = {},
+          children,
+          ...rest
+        } = columnProps;
+        const columnSlots: Recordable = {};
 
         if (slot) {
-          return (
-            <ElTableColumn {...rest}>
-              {{
-                default: (scope: ColumnScope<T>) => <>{slots[slot]?.(scope)}</>,
-              }}
-            </ElTableColumn>
-          );
-        } else if (Array.isArray(children)) {
-          return (
-            <ElTableColumn {...rest}>
-              {{
-                default: () => children.map(child => <Column {...child} />),
-              }}
-            </ElTableColumn>
-          );
+          columnsSlots.default = slot;
         }
-        return <ElTableColumn {...rest} />;
+        Object.keys(columnsSlots).forEach(key => {
+          const slotName: TableColumnSlotName = Reflect.get(columnsSlots, key);
+
+          if (slots[slotName]) {
+            columnSlots[key] = (scope: any) => <>{slots[slotName]?.(scope)}</>;
+          }
+        });
+
+        if (Array.isArray(children) && children.length > 0) {
+          columnSlots.default = () =>
+            children.map(child => <Column {...child} />);
+        }
+        return <ElTableColumn {...rest} v-slots={columnSlots} />;
       };
 
-      return () => (
-        <>
-          <ElTable ref={instanceRef} {...optionsState.value.props}>
-            {optionsState.value.columns?.map(column => (
+      return () => {
+        const { columns = [], props } = tableState.value;
+
+        return (
+          <ElTable ref={tableInstance} {...props}>
+            {columns.map(column => (
               <Column {...column} />
             ))}
           </ElTable>
-        </>
-      );
+        );
+      };
     },
   });
 
-  return [Table, instanceActions] as const;
+  return [Table, tableController] as const;
 }
