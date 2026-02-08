43e743b032f0cd4de6520100839daaa213fb3694
Yuki <admin@yuki.sh>
2026-02-08 09:07:52 +0800

    docs: add useDialog docs and sidebar entry

---
+++ docs/examples/dialog/BasicUsage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { useDialog } from 'element-hooks';
+  import { ElMessageBox } from 'element-plus';
+
+  const [Dialog, { open, close }] = useDialog({
+    width: 500,
+    title: 'Tips',
+    async beforeClose(done) {
+      try {
+        await ElMessageBox.confirm('Are you sure to close this dialog?');
+        done();
+      } catch {}
+    },
+  });
+</script>
+
+<template>
+  <el-button plain @click="open">Click to open the Dialog</el-button>
+  <Dialog>
+    <span>This is a message</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="close">Cancel</el-button>
+        <el-button type="primary" @click="close">Confirm</el-button>
+      </div>
+    </template>
+  </Dialog>
+</template>
+
+<style scoped lang="scss"></style>
