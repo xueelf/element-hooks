fb10b64eade3ced3b7a3b3b9dc3dc9b76d3dc9a4
Yuki <admin@yuki.sh>
2026-02-09 11:41:33 +0800

    fix(docs): preserve theme import order

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,9 @@
 import 'virtual:uno.css';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
-import './custom.scss';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import './custom.scss';
 
 export default {
   extends: DefaultTheme,
