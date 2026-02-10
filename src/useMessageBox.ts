d6936ae6fe0cbdd99029da36ed30efcd1723b827
Yuki <admin@yuki.sh>
2026-02-10 09:50:22 +0800

    fix(message-box): correct parameter types

--- src/useMessageBox.ts
+++ src/useMessageBox.ts
@@ -4,6 +4,13 @@ import {
   ElMessageBox,
 } from 'element-plus';
 
+type MessageBoxMethodParams = ElMessageBoxShortcutMethod extends {
+  (...args: infer A): unknown;
+  (...args: infer B): unknown;
+}
+  ? A | B
+  : never;
+
 /**
  * ElMessageBox 会在点击关闭时返回一个 rejected 的 Promise，导致抛出异常。
  * 这里对 confirm 和 prompt 方法进行了封装，方便直接使用 await 处理。
@@ -11,22 +18,20 @@ import {
 export function useMessageBox() {
   return {
     alert: ElMessageBox.alert,
-    confirm: async (
-      ...args: Parameters<ElMessageBoxShortcutMethod>
-    ): Promise<boolean> => {
+    confirm: async (...args: MessageBoxMethodParams): Promise<boolean> => {
       try {
-        await ElMessageBox.confirm(...args);
+        await Reflect.apply(ElMessageBox.confirm, null, args);
         return true;
       } catch {
         return false;
       }
     },
-    prompt: async (
-      ...args: Parameters<ElMessageBoxShortcutMethod>
-    ): Promise<string | null> => {
+    prompt: async (...args: MessageBoxMethodParams): Promise<string | null> => {
       try {
-        const { value } = <MessageBoxInputData>(
-          await ElMessageBox.prompt(...args)
+        const { value }: MessageBoxInputData = await Reflect.apply(
+          ElMessageBox.prompt,
+          null,
+          args,
         );
         return value;
       } catch {
