0e36ecad0b50165298cdba3e0ce85786b742e1ae
Yuki <admin@yuki.sh>
2026-02-12 15:08:37 +0800

    chore: add '@' alias and SCSS globals to docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,10 @@
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+
 import 'virtual:uno.css';
 import 'element-plus/dist/index.css';
 import 'element-plus/theme-chalk/dark/css-vars.css';
-import type { Theme } from 'vitepress';
-import DefaultTheme from 'vitepress/theme';
-import './custom.scss';
+import './styles/index.scss';
 
 export default {
   extends: DefaultTheme,
