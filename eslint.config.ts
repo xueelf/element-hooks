5c3836b37212fe0c3243f38eb5b898420b24a68f
Yuki <admin@yuki.sh>
2026-02-27 16:46:44 +0800

    build: add Vue linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'eslint/config';
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from 'eslint-config-prettier/flat';
 
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,ts,tsx}'],
+  },
+
+  globalIgnores(['**/lib/**']),
+
+  ...pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  skipFormatting,
+
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unsafe-function-type': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/no-reserved-component-names': 'off',
     },
   },
 );
