17daa477e0a59cf6eebbf0f6f555c8a5d7e2910f
Yuki <admin@yuki.sh>
2026-02-28 14:35:18 +0800

    feat: add default export for all hooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+import { useDialog } from './useDialog';
+import { useForm } from './useForm';
+import { useMessage } from './useMessage';
+import { useMessageBox } from './useMessageBox';
+import { useTable } from './useTable';
+
+export default {
+  useDialog,
+  useForm,
+  useMessage,
+  useMessageBox,
+  useTable,
+};
+
 export * from './useDialog';
 export * from './useForm';
+export * from './useMessage';
 export * from './useMessageBox';
 export * from './useTable';
