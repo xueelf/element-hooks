6ff6a0aecf4fe6ad675001cde508a533f7129b34
Yuki <admin@yuki.sh>
2026-03-04 17:23:56 +0800

    docs: format ex-table code examples

--- docs/examples/ex-table/CustomDataMap.vue
+++ docs/examples/ex-table/CustomDataMap.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
-import { useExTable } from 'element-hooks';
+  import { useExTable } from 'element-hooks';
 
-const [ExTable] = useExTable({
-  data: {
-    records: [
-      { date: '2016-05-03', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
-      { date: '2016-05-02', name: 'Tom', address: 'No. 189, Grove St, Los Angeles' },
+  const [ExTable] = useExTable({
+    data: {
+      records: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ],
+      count: 200,
+    },
+    columns: [
+      { prop: 'date', label: 'Date', width: '180' },
+      { prop: 'name', label: 'Name', width: '180' },
+      { prop: 'address', label: 'Address' },
     ],
-    count: 200,
-  },
-  columns: [
-    { prop: 'date', label: 'Date', width: '180' },
-    { prop: 'name', label: 'Name', width: '180' },
-    { prop: 'address', label: 'Address' },
-  ],
-  pagination: {
-    currentPage: 1,
-    pageSize: 10,
-    layout: 'prev, pager, next',
-    props: {
+    pagination: {
+      currentPage: 1,
+      pageSize: 10,
+      layout: 'prev, pager, next',
+      props: {
         result: 'records',
-        total: 'count'
-    }
-  },
-});
+        total: 'count',
+      },
+    },
+  });
 </script>
 
 <template>
