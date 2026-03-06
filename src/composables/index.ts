233a5ad8b38bdd2a240189208d33449f8a25a381
Yuki <admin@yuki.sh>
2026-03-06 11:10:05 +0800

    docs: add auto-import guide and simplify default exports

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,3 +1,19 @@
+import { useDialog } from './core/dialog';
+import { useForm } from './core/form';
+import { useMessage } from './core/message';
+import { useMessageBox } from './core/message-box';
+import { useTable } from './core/table';
+import { useExTable } from './extra/ex-table';
+
+export default {
+  useDialog,
+  useForm,
+  useMessage,
+  useMessageBox,
+  useTable,
+  useExTable,
+};
+
 export * from './core/dialog';
 export * from './core/form';
 export * from './core/message';
