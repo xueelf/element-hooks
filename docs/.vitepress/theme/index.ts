0122d9b156c9f8981bd8beeb91d651215c0cfa45
Yuki <admin@yuki.sh>
2026-08-16 15:20:01 +0800

    fix(docs): provide Element Plus injection keys

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,8 @@
 import ElementHooks from 'element-hooks';
-import ElementPlus from 'element-plus';
+import ElementPlus, {
+  ID_INJECTION_KEY,
+  ZINDEX_INJECTION_KEY,
+} from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import { type Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
@@ -12,6 +15,8 @@ import './styles/index.scss';
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    app.provide(ID_INJECTION_KEY, { prefix: 1024, current: 0 });
+    app.provide(ZINDEX_INJECTION_KEY, { current: 0 });
     app.use(ElementPlus, {
       locale: zhCn,
     });
